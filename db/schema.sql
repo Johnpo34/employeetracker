63461c4b1457c6cb7a1e5bde5893fadaa673bc0c
John Pohl <johnpohl34@gmail.com>
2022-02-21 09:59:09 -0500

    worked on schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,27 @@
-DROP DATABASE IF EXISTS
-CREATE DATABASE
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE
+USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE roles (
-    --id is primary key
-    id INT NOT NULL
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    -- id is primary key
-    id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    FOREIGN KEY role_id REFERENCES role(id),
+    manager_id INT NOT NULL,
+    FOREIGN KEY manager_id REFERENCES employee(id)
 );
