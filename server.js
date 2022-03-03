8208ae7f8883ab28233b6a45db39ef426778132b
John Pohl <johnpohl34@gmail.com>
2022-03-02 19:45:25 -0500

    added queries

--- server.js
+++ server.js
@@ -27,7 +27,13 @@ function addEmployee() {
       name: 'Employee name',
       message: 'Employees name?'
     }
-  ]) .then(answers => {})
+  ]).then(answers => {
+    db.query('INSERT INTO employee (name) VALUES')
+    ('${answer.EmployeeName}'), (err, res) => {
+      if (err) throw err;
+      console.log('$(answers.EmployeeName} added to the database');
+    }
+  });
 
   function addDepartment() {
     inquirer.prompt([
@@ -36,7 +42,13 @@ function addEmployee() {
         name: 'Department Name',
         message: 'Name of department?'
       }
-    ]) .then(answers => {})
+    ]).then(answers => {
+      db.query('INSERT INTO departments (name) VALUES')
+        ('${answer.DepartmentName}'), (err, res) => {
+          if (err) throw err;
+          console.log('${answers.DepartmentName} added to the database.');
+        }
+    });
 
     function addRole() {
       inquirer.prompt([
@@ -45,59 +57,41 @@ function addEmployee() {
           name: 'Role name',
           message: 'Name of role?'
         }
-      ]) .then(answers => {})
-    
-// insert query to add data to employee table
-function init() {
-    inquirer.prompt([
-        {
+      ]).then(answers => {
+        db.query("INSERT INTO roles (name) VALUES")
+          ('${answer.RoleName}'), (err, res) => {
+            if (err) throw err;
+            console.log('$answers.RoleName} added to the database.');
+          }
+      });
+
+      // insert query to add data to employee table
+      function init() {
+        inquirer.prompt([
+          {
             type: 'list',
             name: 'mainQuestion',
             message: 'What would you like to do?',
             choices: ['View All', 'View Employees', 'View Departments', 'View Roles', 'Add Employee', 'Add Department', 'Add Role', 'Update Employees Role']
-        }
-      ]).then(answer => {
-        if(answer.mainQuestion === 'View All') {
-          viewAll()
-        } else if (answer.mainQuestion === 'View Employees') {
-          viewEmployee()
-        } else if (answer.mainQuestion === 'View Departments') {
-          viewDepartment()
-        } else if (answer.mainQuestion === 'View Roles') {
-          viewRoles()
-        } else if (answer.mainQuestion === 'Add Employee') {
-          addEmployee()
-        } else if (answer.mainQuestion === 'Add Department') {
-          addDepartment()
-        } else if (answer.mainQuestion === 'Add Role') {
-          addRole()
-        } else if (answer.mainQuestion === 'Update Emplyees Role')
-          updateEmployeeRole()
-      })
-};
+          }
+        ]).then(answer => {
+          if (answer.mainQuestion === 'View All') {
+            viewAll()
+          } else if (answer.mainQuestion === 'View Employees') {
+            viewEmployee()
+          } else if (answer.mainQuestion === 'View Departments') {
+            viewDepartment()
+          } else if (answer.mainQuestion === 'View Roles') {
+            viewRoles()
+          } else if (answer.mainQuestion === 'Add Employee') {
+            addEmployee()
+          } else if (answer.mainQuestion === 'Add Department') {
+            addDepartment()
+          } else if (answer.mainQuestion === 'Add Role') {
+            addRole()
+          } else if (answer.mainQuestion === 'Update Emplyees Role')
+            updateEmployeeRole()
+        })
+      };
 
-db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, '', manager.last_name) AS manager FROM employee LEFT JOIN role ON emplyee_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;")
-
-// what would you like to do?
-    // view all employees -- show table employee data, enployee ids, first name, last name, job titles, departments, salaries, their manager
-
-    // view all departments -- show table with departments and ids
-
-    // view all roles -- show job title, role id, department role, salary
-
-    // add department -- prompt departments
-
-    // add role -- name, salary, department
-
-    // add emplyee -- first name, last name, employees role, employees manager
-    
-    // update employee role -- new role 
-
-    // quit
-
-
-    // update managers
-    // view emplyees by manager
-    // view emplyees by department
-    // delete departments, roles and employees
-    // view salaries of departments
+      db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, '', manager.last_name) AS manager FROM employee LEFT JOIN role ON emplyee_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;")
